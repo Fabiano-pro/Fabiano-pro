7c95a428e6bff075629060b9777107880396800c
Fabiano-pro <fabienkpetchekou5@gmail.com>
2025-09-23 16:27:50 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,20 +1,22 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-  import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-  const firebaseConfig = {
-    apiKey: "TON_API_KEY",
-    authDomain: "ton_projet.firebaseapp.com",
-    projectId: "ton_projet",
-    storageBucket: "ton_projet.appspot.com",
-    messagingSenderId: "xxxxxxx",
-    appId: "xxxxxxxx"
-  };
+// firebase.js (fichier à mettre à la racine du repo)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-  export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
-  export const storage = getStorage(app);
-</script>
+// 🔁 Remplace uniquement l'objet ci-dessous par celui fourni par Firebase
+const firebaseConfig = {
+  apiKey: "TA_API_KEY_ICI",
+  authDomain: "ton-projet.firebaseapp.com",
+  projectId: "ton-projet",
+  storageBucket: "ton-projet.appspot.com",
+  messagingSenderId: "TON_SENDER_ID",
+  appId: "TON_APP_ID"
+};
+
+// Initialisation Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
